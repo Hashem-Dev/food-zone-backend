fb5de2870df33045fb66674245f823fc595b256c
Hashem-Dev <devhashem25@gmail.com>
2024-10-23 14:10:42 +0300

    Init Server

---
+++ config/cors-config.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+
+const whitelist = ["http://localhost:5000"];
+
+corsOptions = {
+  origin: function (origin, cb) {
+    if (whitelist.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+module.exports = cors(corsOptions);
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("./config/cors-config");
+
+dotenv.config({ path: "config.env" });
+
+const port = process.env.PORT || 5000;
+const databaseUrl = process.env.DB_URL;
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors);
+// psi6w7kdtu44mzs
+mongoose
+  .connect(databaseUrl)
+  .then((connect) => {
+    console.log("Connect to database: " + connect.connection.name);
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+});
